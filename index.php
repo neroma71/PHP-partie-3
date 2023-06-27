3f11144e2e15f74c97bbafd496c0556784459bd2
neroma71 <neroma71@gmail.com>
2023-06-27 16:54:46 +0200

    end exo3

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$i = 1;
+while($i <= 10){
+    echo $i++." ";
+}
+echo "<br />";
+
+$n1 = 0;
+$n2 = 10;
+while($n1 <= 20){
+    $n1 * $n2;
+    echo $n1++;
+}
+echo "<br />";
+
+$num1 = 100;
+$num2 = 10;
+while($num1 >= 10){
+    $num1 * $num2;
+    echo $num1."<br />";
+    echo $num1--;
+}
+echo "<br />";
+
+for($a = 1; $a <= 10; $a++){
+    echo $a;
+}
+echo "<br />";
+for($i = 1; $i < 15; $i++){
+    echo "on y arrive ";
+}
+?>
